343c2ae972efc83e09cacb8437499a15de6b8d70
wzz1190 <79913593+wzz1190@users.noreply.github.com>
2021-04-11 12:55:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/lixuande/rt2860v2 package/rt2860v2
